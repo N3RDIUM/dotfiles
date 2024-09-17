15968e716617c9fdcbb100fcf7b27cd0b42f3e76
N3RDIUM <n3rdium@gmail.com>
2024-09-17 07:33:30 +0530

    Adding the flaky stuff

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "N3RDIUM's Flake!";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+	config.allowUnfree = true;
+      };
+      lib = nixpkgs.lib;
+
+    in {
+      nixosConfigurations.n3rdium = lib.nixosSystem rec {
+        inherit system;
+	modules = {
+          ./nixos/configuration.nix
+	  home-manager.nixosModules.home-manager
+	  {
+            home-manager.useGlobalPkgs = true;
+	    home-manager.useUserPkgs = true;
+	    home-manager.users.n3rdium = import ./home.nix ;
+            home-manager.extraSpecialArgs = specialArgs;
+	  }
+	};
+      };
+      #homeConfigurations."n3rdium" = home-manager.lib.homeManagerConfiguration {
+      #  inherit pkgs;
+#
+#        # Specify your home configuration modules here, for example,
+#        # the path to your home.nix.
+#        modules = [ 
+#	  ./home.nix
+#          ./configuration.nix
+#        ];
+#
+#        # Optionally use extraSpecialArgs
+#        # to pass through arguments to home.nix
+#      };
+    };
+}
