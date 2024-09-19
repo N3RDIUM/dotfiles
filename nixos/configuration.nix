def2fc9841a3d27ce06124bb9bb92964555c6083
n3rdium <n3rdium@gmail.com>
2024-09-19 20:24:10 +0530

    Oops again!

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -10,9 +10,8 @@
   ];
 
   # Bootloader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda";
-  boot.loader.grub.useOSProber = true;
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
