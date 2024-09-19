7d1b476ebf5ba670579d97f79a1ffeb31491760a
n3rdium <n3rdium@gmail.com>
2024-09-19 20:37:46 +0530

    NVIDAIAAASIAIDAIAIIAIAIADIAA

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -47,7 +47,22 @@
   services.displayManager.sddm.enable = true;
   services.displayManager.sddm.wayland.enable = true;
   programs.hyprland.enable = true;
+  
+  # Nvidia stuff
+  hardware.opengl = {
+    enable = true;
+  };
 
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    open = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
 
   # Configure keymap in X11
   services.xserver.xkb = {
