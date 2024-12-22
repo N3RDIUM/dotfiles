da9152ea5a0e85c123a17e91502294685df61a4c
n3rdium <n3rdium@gmail.com>
2024-12-22 16:15:06 +0530

    Better wallpaper thing

--- wallpapers/wallpaper.py
+++ wallpapers/wallpaper.py
@@ -1,13 +1,11 @@
 import os
 import random
 import subprocess
-import time
 
 wallpapers = os.listdir(os.path.dirname(__file__))
 wallpapers.remove("wallpaper.py")
 
 subprocess.call("swww-daemon")
-
-while True:
-    os.system(f"swww img ~/wallpapers/{random.choice(wallpapers)}")
-    time.sleep(42)
+os.system(
+    f"swww img {os.path.join(os.path.dirname(__file__), random.choice(wallpapers))}"
+)
